a1126660bad25d839f9ea4cf540201df886251af
Siri Surya <sirisurya2007@gmail.com>
2026-06-27 20:34:49 +0530

    added redirects

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://ratehub-backend-knt7.onrender.com/api';
+const API_URL = 'https://ratehub-backend-knt7.onrender.com';
 
 const api = axios.create({
   baseURL: API_URL,
